6e04ae76c26d51cd0a0b3a9bcdddbda94db0eac5
Henrik Bellander <henrik.bellander@gmail.com>
2019-11-06 13:41:16 +0100

    Player movement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
@@ -9,15 +11,46 @@ public class Main {
     Random r = new Random();
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();      //Skapar terminalfönster
         Terminal terminal = terminalFactory.createTerminal();
 
-        Player player = new Player ();
+        Player player = new Player ();                                              //Skapar spelare
         terminal.setCursorVisible(false);
-        terminal.setCursorPosition(player.getX(), player.getY());
         terminal.setForegroundColor(TextColor.ANSI.WHITE);
 
-        terminal.putCharacter(player.getPlayerChar());
+
+        do {
+            terminal.clearScreen();
+            terminal.setCursorPosition(player.getX(), player.getY());
+            terminal.putCharacter(player.getPlayerChar());
+            terminal.flush();
+
+            KeyStroke keyStroke = null;
+            do {
+                Thread.sleep(5); // might throw InterruptedException
+                keyStroke = terminal.pollInput();
+            } while (keyStroke == null);
+
+            KeyType type = keyStroke.getKeyType();
+
+            switch (type) {                                     //Vår förflyttning
+                case ArrowDown:
+                    if (player.getY() == 23) {
+                    } else {
+                        player.setY(player.getY() + 1);
+                    }
+                    break;
+                case ArrowUp:
+                    if (player.getY() == 0) {
+                    } else {
+                        player.setY(player.getY() -1);
+                    }
+                    break;
+            }
+            terminal.flush();
+
+        } while (true);
 
     }
 }
