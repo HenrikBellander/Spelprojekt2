9eafc413dd8b66b4baacc6e505f1964c264041a2
Henrik Bellander <henrik.bellander@gmail.com>
2019-11-06 13:16:26 +0100

    Skapa player

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,18 @@
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
+import java.util.Random;
 
 public class Main {
+
+    Random r = new Random();
 
     public static void main(String[] args) throws IOException, InterruptedException {
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
         Terminal terminal = terminalFactory.createTerminal();
+
+        Player player = new Player ();
+
     }
 }
 
---
+++ src/main/java/Player.java
@@ -0,0 +1,16 @@
+public class Player {
+    private final int x = 10;
+    private int y = 12;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
